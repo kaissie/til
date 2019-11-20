3aa77f261f89844f7fe7b6d87d1ffefa431fc918
Kaisei <k5746491@kadai.jp>
2019-11-21 00:09:16 +0900

    mini-fix xorshift.cpp

--- Algorithms/xorshift.cpp
+++ Algorithms/xorshift.cpp
@@ -3,26 +3,21 @@
 
 class XorShift {
 private:
-  struct dictionary {
-    uint32_t x;
-    uint32_t y;
-    uint32_t z;
-    uint32_t w;
-  };
+  struct dictionary { uint32_t x; uint32_t y; uint32_t z; uint32_t w; };
+  uint32_t x;
+  uint32_t y;
+  uint32_t z;
+  uint32_t w;
+  uint32_t t;
 public:
-  static const struct dictionary defaults = {
-    123456789,
-    362436069,
-    521288629,
-    88675123
-  };
+  static const struct dictionary defaults;
   uint32_t rand_count = 0;
   struct dictionary seeds;
 
-  XorShift(uint32_t w=time(nullptr), uint32_t x=NULL, uint32_t y=NULL,uint32_t z=NULL) {
-    x = x != NULL ? x : w<<13;
-    y = y != NULL ? y : (w>>9)^(x<<6);
-    z = z != NULL ? z : y>>7;
+  XorShift(uint32_t w=defaults.w, uint32_t x=defaults.x, uint32_t y=defaults.y,uint32_t z=defaults.z) {
+    x = x != 0 ? x : w<<13;
+    y = y != 0 ? y : (w>>9)^(x<<6);
+    z = z != 0 ? z : y>>7;
     seeds={x,y,z,w};
     this->w=w;this->x=x;this->y=y;this->z=z;
   }
@@ -35,16 +30,19 @@ public:
     z = w;
     return w = (w^(w>>19)) ^ (t^(t>>8));
   }
-
-  static inline XorShift defaultSeed(){
-    return XorShift(defaults.w, defaults.x, defaults.y, defaults.z);
-  }
-
+};
+const struct XorShift::dictionary XorShift::defaults={
+  123456789,
+  362436069,
+  521288629,
+  88675123
 };
 
-
 int main(int argc, char const *argv[]) {
-  auto r_def=XorShift::defaultSeed();
-
+  auto r=XorShift();
+  std::cout << "XORShift" << '\n';
+  for (size_t i = 0; i < 10; i++) {
+    std::cout << r.rand() << '\n';
+  }
   return 0;
 }
